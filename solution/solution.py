af227dab35aea92fc1bf1e36056aae221021da80
Pedro Ramos <ramosct70@hotmail.com>
2020-02-07 14:17:20 -0300

    Atualização

--- solution/solution.py
+++ solution/solution.py
@@ -10,25 +10,98 @@ import random
 w, h = [int(i) for i in input().split()]
 n = int(input())  # maximum number of turns before game over.
 x0, y0 = [int(i) for i in input().split()]
+janelaBatman = [x0, y0]
+
+janelas = []
+janelax = 0
+janelay = 0
+janelasRetiradas = 0
+
+for i in range(w * h):
+    janelas.append((janelax, janelay))
+    if janelax < w - 1:
+        janelax += 1
+    else:
+        janelay += 1
+        janelax = 0
 
 # game loop
 while True:
     bomb_dir = input()
-    janelas = []
-    janelax = 0
-    janelay = 0
+    janelasNovas = []
 
-    for i in range((w - 1) * (h - 1)):
-        janelas.append((janelax, janelay))
-        if janelax < w - 1:
-            janelax += 1
-        else:
-            janelay += 1
-            janelax = 0
+    # Pega a direção e mostra todas as janelas naquela direção
+    # Y axis
+    if bomb_dir == "U":
+        for i in range(w * h):
+            janelaDirecao = janelas[i - janelasRetiradas]
+            if janelaDirecao[1] < janelaBatman[1] and janelaDirecao[0] == janelaBatman[0]:
+                janelasNovas.append(janelaDirecao)
+                janelas.remove(janelaDirecao)
+                janelasRetiradas += 1
 
-    for i in range(len(janelas)):
-        # Pega a direção e mostra todas as janelas naquela direção
-        pass
+    if bomb_dir == "D":
+        for i in range(w * h):
+            janelaDirecao = janelas[i - janelasRetiradas]
+            if janelaDirecao[1] > janelaBatman[1] and janelaDirecao[0] == janelaBatman[0]:
+                janelasNovas.append(janelaDirecao)
+                janelas.remove(janelaDirecao)
+                janelasRetiradas += 1
+    # X axis
+    if bomb_dir == "R":
+        for i in range(w * h):
+            janelaDirecao = janelas[i - janelasRetiradas]
+            if janelaDirecao[1] == janelaBatman[1] and janelaDirecao[0] > janelaBatman[0]:
+                janelasNovas.append(janelaDirecao)
+                janelas.remove(janelaDirecao)
+                janelasRetiradas += 1
 
-    janela = random.choice(janelas)
+    if bomb_dir == "L":
+        for i in range(w * h):
+            janelaDirecao = janelas[i - janelasRetiradas]
+            if janelaDirecao[1] == janelaBatman[1] and janelaDirecao[0] < janelaBatman[0]:
+                janelasNovas.append(janelaDirecao)
+                janelas.remove(janelaDirecao)
+                janelasRetiradas += 1
+
+    # XY axis
+    if bomb_dir == "UR":
+        for i in range(w * h):
+            janelaDirecao = janelas[i - janelasRetiradas]
+            if janelaDirecao[1] < janelaBatman[1] and janelaDirecao[0] > janelaBatman[0]:
+                janelasNovas.append(janelaDirecao)
+                janelas.remove(janelaDirecao)
+                janelasRetiradas += 1
+
+    if bomb_dir == "UL":
+        for i in range(w * h):
+            janelaDirecao = janelas[i - janelasRetiradas]
+            if janelaDirecao[1] < janelaBatman[1] and janelaDirecao[0] < janelaBatman[0]:
+                janelasNovas.append(janelaDirecao)
+                janelas.remove(janelaDirecao)
+                janelasRetiradas += 1
+
+    if bomb_dir == "DR":
+        for i in range(w * h):
+            janelaDirecao = janelas[i - janelasRetiradas]
+            if janelaDirecao[1] > janelaBatman[1] and janelaDirecao[0] > janelaBatman[0]:
+                janelasNovas.append(janelaDirecao)
+                janelas.remove(janelaDirecao)
+                janelasRetiradas += 1
+
+    if bomb_dir == "DL":
+        for i in range(w * h):
+            janelaDirecao = janelas[i - janelasRetiradas]
+            if janelaDirecao[1] > janelaBatman[1] and janelaDirecao[0] < janelaBatman[0]:
+                janelasNovas.append(janelaDirecao)
+                janelas.remove(janelaDirecao)
+                janelasRetiradas += 1
+
+    try:
+        janela = janelasNovas[int((len(janelasNovas) - 1) / 2)]
+
+    except IndexError:
+        janela = janelaBatman
+
     print(janela[0], janela[1])
+    janelaBatman = janela
