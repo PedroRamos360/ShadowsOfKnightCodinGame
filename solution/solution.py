9e14cd392712b296c0a78c08e39b5270df648427
Pedro Ramos <ramosct70@hotmail.com>
2020-02-08 14:56:14 -0300

    Atualização

--- solution/solution.py
+++ solution/solution.py
@@ -15,7 +15,8 @@ janelaBatman = [x0, y0]
 janelas = []
 janelax = 0
 janelay = 0
-janelasRetiradas = 0
+
+# Criar dicionário para contar direções aqui
 
 for i in range(w * h):
     janelas.append((janelax, janelay))
@@ -34,79 +35,56 @@ while True:
     # Y axis
     if bomb_dir == "U":
         for i in range(w * h):
-            janelaDirecao = janelas[i - janelasRetiradas]
+            janelaDirecao = janelas[i]
             if janelaDirecao[1] < janelaBatman[1] and janelaDirecao[0] == janelaBatman[0]:
                 janelasNovas.append(janelaDirecao)
-                janelas.remove(janelaDirecao)
-                janelasRetiradas += 1
 
     if bomb_dir == "D":
         for i in range(w * h):
-            janelaDirecao = janelas[i - janelasRetiradas]
+            janelaDirecao = janelas[i]
             if janelaDirecao[1] > janelaBatman[1] and janelaDirecao[0] == janelaBatman[0]:
                 janelasNovas.append(janelaDirecao)
-                janelas.remove(janelaDirecao)
-                janelasRetiradas += 1
+
     # X axis
     if bomb_dir == "R":
         for i in range(w * h):
-            janelaDirecao = janelas[i - janelasRetiradas]
+            janelaDirecao = janelas[i]
             if janelaDirecao[1] == janelaBatman[1] and janelaDirecao[0] > janelaBatman[0]:
                 janelasNovas.append(janelaDirecao)
-                janelas.remove(janelaDirecao)
-                janelasRetiradas += 1
 
     if bomb_dir == "L":
         for i in range(w * h):
-            janelaDirecao = janelas[i - janelasRetiradas]
+            janelaDirecao = janelas[i]
             if janelaDirecao[1] == janelaBatman[1] and janelaDirecao[0] < janelaBatman[0]:
                 janelasNovas.append(janelaDirecao)
-                janelas.remove(janelaDirecao)
-                janelasRetiradas += 1
 
     # XY axis
     if bomb_dir == "UR":
         for i in range(w * h):
-            janelaDirecao = janelas[i - janelasRetiradas]
+            janelaDirecao = janelas[i]
             if janelaDirecao[1] < janelaBatman[1] and janelaDirecao[0] > janelaBatman[0]:
                 janelasNovas.append(janelaDirecao)
-                janelas.remove(janelaDirecao)
-                janelasRetiradas += 1
 
     if bomb_dir == "UL":
         for i in range(w * h):
-            janelaDirecao = janelas[i - janelasRetiradas]
+            janelaDirecao = janelas[i]
             if janelaDirecao[1] < janelaBatman[1] and janelaDirecao[0] < janelaBatman[0]:
                 janelasNovas.append(janelaDirecao)
-                janelas.remove(janelaDirecao)
-                janelasRetiradas += 1
 
     if bomb_dir == "DR":
         for i in range(w * h):
-            janelaDirecao = janelas[i - janelasRetiradas]
+            janelaDirecao = janelas[i]
             if janelaDirecao[1] > janelaBatman[1] and janelaDirecao[0] > janelaBatman[0]:
                 janelasNovas.append(janelaDirecao)
-                janelas.remove(janelaDirecao)
-                janelasRetiradas += 1
 
     if bomb_dir == "DL":
         for i in range(w * h):
-            janelaDirecao = janelas[i - janelasRetiradas]
+            janelaDirecao = janelas[i]
             if janelaDirecao[1] > janelaBatman[1] and janelaDirecao[0] < janelaBatman[0]:
                 janelasNovas.append(janelaDirecao)
-                janelas.remove(janelaDirecao)
-                janelasRetiradas += 1
 
-    try:
-        janela = janelasNovas[int(len(janelas) / 2)]
-
-    except IndexError:
-        janela = janelaBatman
+    janela = janelasNovas[int(len(janelasNovas) / 2)]
 
     print(janela[0], janela[1])
     janelaBatman = janela
 
-# Erros:
-# 1 Se mover na diagonal o batman para
-# 2 Se mover muitas vezes da Index Error na linha 37 (ou outras linhas com esse código)
-#
